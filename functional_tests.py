8b77bd27f103c1b3744c468497a01dff38669bf8
yuyu yuan <yuyuyuan@yuyudeMacBook-Pro.local>
2019-03-08 16:04:13 +0800

    Functional test now checkwe can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,20 +20,35 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the page title and header mention to-do lists
 
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-
+        header_text = self.browser.find_element_by_tag_name('h1').text  #(1)
+        self.assertIn('To-Do', header_text)
+ 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item') #(1)
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# She types " Buy peacock feathers" into a text box ( Edith's hobby
-# is tying fly-fishing lures)
+        # She types " Buy peacock feathers" into a text box ( Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_key('Buy peacock feathers') #(2)
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(keys.ENTER) #(3)
+        time.sleep(1) #(4)
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" ( Edith is very methodical)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr') #(1)
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
-# The page updates again, and now shows both items on her list
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" ( Edith is very methodical)
+        self.fail('Finish the test!')
+        # The page updates again, and now shows both items on her list
 
 # Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some
