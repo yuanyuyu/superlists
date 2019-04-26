31951fc81624d8c73d51c358b86800215f19af3e
yuanyuyu <yuanyuyu@bupt.edu.cn>
2019-04-26 20:14:41 +0800

    set STATIC_ROOT in settings and disable admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '/Users/yuyuyuan/Documents/测试教材/2019教程/2019春软件测试硕士/1初学/superlists/lists/static'))
+
+                                           
+
