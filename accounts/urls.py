f05597b9d4131c47874b392147bdde6d6be070b0
yuanyuyu <yuanyuyu@bupt.edu.cn>
2020-02-02 13:27:34 +0800

    Custom passwordless auth backend + custom user model

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from lists import views as list_views
-from lists import urls as list_urls
-from accounts import urls as accounts_urls
+from django.conf.urls import url
+from accounts import views
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
-    url(r'^lists/', include(list_urls)),
-    url(r'^accounts/', include(accounts_urls)),
+    url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
+    url(r'^login$', views.login, name='login'),
  ]
--- superlists/settings.py
+++ superlists/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+    ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,6 +127,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+EMAIL_HOST = 'smtp.163.com'
+EMAIL_HOST_USER = 'yuanyuyu_1971@163.com'
+EMAIL_HOST_PASSWORD = '591100yuanyuyu'
+EMAIL_PORT = 25
+EMAIL_USE_TLS = True
 
                                            
 
